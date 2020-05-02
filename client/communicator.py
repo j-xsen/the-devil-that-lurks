900653aa73586d344c5fa83aaa9412d1e53f99d9
Jaxsen Honeycutt <jaxsenhoneycutt@gmail.com>
2020-05-02 19:31:42 -0400

    remove some files that arent supposed to be there....

--- client/communicator.py
+++
@@ -1,9 +0,0 @@
-# creates datagrams to send to server
-from direct.distributed import PyDatagram
-
-REQUEST_GAME = 1
-
-def request_game:
-    dg = PyDatagram()
-    dg.addUint8(REQUEST_GAME)
-    return dg
